aedc0190982c4bf767f62071cc064f3b751eabf0
Eric Manosalva <ramses.manosalva@gmail.com>
2022-07-23 10:37:10 -0500

    feat: add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './common/database/database.module';
+import { GraphqlModule } from './common/graphql/graphql.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { AccountsModule } from './modules/accounts/accounts.module';
+import { MovementsModule } from './modules/movements/movements.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule, GraphqlModule, CategoriesModule, AccountsModule, MovementsModule],
   controllers: [AppController],
   providers: [AppService],
 })
