5f0988bfa34cecaf477e156d127cebf1ab78aefe
Eric Manosalva <ramses.manosalva@gmail.com>
2022-07-23 10:33:53 -0500

    chore: remove modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './common/database/database.module';
-import { GraphqlModule } from './common/graphql/graphql.module';
-import { CategoriesModule } from './modules/categories/categories.module';
-import { AccountsModule } from './modules/accounts/accounts.module';
-import { MovementsModule } from './modules/movements/movements.module';
 
 @Module({
-  imports: [DatabaseModule, GraphqlModule, CategoriesModule, AccountsModule, MovementsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
